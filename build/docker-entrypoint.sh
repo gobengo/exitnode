356ac596d0be1142ec1e213bcbe78cf2848ba284
Benjamin Goering <bengoering@gmail.com>
2018-02-21 20:39:09 -0800

    explain files

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+usage () {
+cat <<HELP_USAGE
+$0
+
+Docker entrypoint. Configures services for PUBLIC_IP env var, starts services.
+HELP_USAGE
+}
+
 # print each line
 # set -x
 
 # exit on error
 set -e
 
-./build/bin/set-public-ip "$EXITNODE_PUBLIC_IP"
+./build/bin/set-public-ip "$PUBLIC_IP"
 
 service sudomesh-gateway start
 service tunneldigger start
